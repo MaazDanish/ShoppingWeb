ef13bcf50430d12e7d26cd73fda45b3fbfb35314
MaazDanish <dk599318@gmail.com>
2023-09-02 10:57:47 +0530

    conncted with database using sequelized

--- models/product.js
+++ models/product.js
@@ -1,35 +1,32 @@
-// const Cart = require('./cart');
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-// id is not giving by default or as a arguments .it will be created by randomk method but when we need to update 
-// products details then on clicking we will get an id as argument to match the data which stored in file
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
 
-  save() {
-    return db.execute('INSERT INTO products (title,price,imageUrl,description) values(?,?,?,?)',
-      [this.title, this.price, this.imageUrl, this.description]);
-  }
-  // updatedSave(id){
-  //   return db.execute('INSERT INTO products (id,title,price,imageUrl,description) values(?,?,?,?,?)',
-  //     [this.id,this.title, this.price, this.imageUrl, this.description]);
-  // }
+});
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?',[id]);
-  }
-
-  static deleteProductById(id) {
-
-  }
-};
+module.exports = Product;
